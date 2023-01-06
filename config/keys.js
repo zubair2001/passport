0003a2e767a6b07115558ff4f126ff48ebeedfe0
zubair2001 <zubair_mansuri2001@outlook.com>
2023-01-06 23:54:44 +0500

    key fixed

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://admin:admin@cluster0.icw3hdh.mongodb.net/passportAuth?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://'username':'password'@cluster0.icw3hdh.mongodb.net/passportAuth?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
